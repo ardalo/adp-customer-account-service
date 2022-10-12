f7e15a9b0e0a0eda6d75e9a6f4bd5c4685015c05
Torsten Blasche <torsten.blasche@obi.de>
2022-10-12 22:45:43 +0200

    Fixed class name

--- src/main/java/com/ardalo/customeraccount/CustomerAccountServiceApplication.java
+++ src/main/java/com/ardalo/customeraccount/CustomerAccountServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBlueprintApplication {
+public class CustomerAccountServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBlueprintApplication.class, args);
+		SpringApplication.run(CustomerAccountServiceApplication.class, args);
 	}
 }
