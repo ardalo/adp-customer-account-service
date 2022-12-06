4be9a4ab38a6617963dd037e123fd0b39d74956c
Torsten <email@t-blasche.de>
2022-12-06 23:40:48 +0100

    404 error page

--- src/main/java/com/ardalo/customeraccount/errorpages/PageNotFoundController.java
+++ src/main/java/com/ardalo/customeraccount/errorpages/PageNotFoundController.java
@@ -1,7 +1,5 @@
 package com.ardalo.customeraccount.errorpages;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,8 +9,6 @@ public class PageNotFoundController {
 
   @GetMapping("/api/pages/404")
   public ModelAndView notFoundPage() {
-    ModelAndView modelAndView = new ModelAndView("notFoundPage");
-    modelAndView.getModel().put("currentTimestamp", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()));
-    return modelAndView;
+    return new ModelAndView("notFoundPage");
   }
 }
