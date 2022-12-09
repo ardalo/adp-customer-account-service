400decc3561a617291934cd2ed7e69d423cecabf
Torsten <email@t-blasche.de>
2022-12-09 13:48:21 +0100

    capture original path on 404

--- src/main/java/com/ardalo/customeraccount/errorpages/PageNotFoundController.java
+++ src/main/java/com/ardalo/customeraccount/errorpages/PageNotFoundController.java
@@ -1,6 +1,8 @@
 package com.ardalo.customeraccount.errorpages;
 
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,9 +10,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class PageNotFoundController {
 
   @GetMapping("/api/pages/404")
-  public ModelAndView notFoundPage() {
+  public ModelAndView notFoundPage(@RequestParam(name = "originalPath", required = false) Optional<String> originalPath) {
     ModelAndView modelAndView = new ModelAndView("notFoundPage");
-    modelAndView.getModel().put("originalPath", "not implemented yet");
+    modelAndView.getModel().put("originalPath", originalPath.orElse("n/a"));
     return modelAndView;
   }
 }
