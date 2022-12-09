a7c4de427ea4eb8abad1fb38fa9476dc482e6b87
Torsten <email@t-blasche.de>
2022-12-09 15:10:16 +0100

    make global 404 working again

--- src/main/java/com/ardalo/customeraccount/errorpages/Global404Controller.java
+++ src/main/java/com/ardalo/customeraccount/errorpages/Global404Controller.java
@@ -1,7 +1,6 @@
 package com.ardalo.customeraccount.errorpages;
 
 import java.util.Optional;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +13,12 @@ public class Global404Controller {
    * Header "X-Replaced-Path" is set by Traefik.
    * See https://doc.traefik.io/traefik/middlewares/http/replacepath/#general
    * and /deployment/helm/values.yaml for details.
+   * Returns HTTP 200 OK. This is mapped to HTTP 404 Not Found within Traefik Middleware.
    */
   @GetMapping("/api/pages/global-404")
   public ModelAndView global404Page(@RequestHeader(name = "X-Replaced-Path", required = false) Optional<String> originalPath) {
     ModelAndView modelAndView = new ModelAndView("global404Page");
     modelAndView.getModel().put("originalPath", originalPath.orElse("n/a"));
-    modelAndView.setStatus(HttpStatus.NOT_FOUND);
     return modelAndView;
   }
 }
